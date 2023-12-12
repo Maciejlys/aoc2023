9ce83ebb63e048560c24e9a00fed785f6e49106d
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2023-12-12 02:50:53 +0100

    refactor test

--- src/11/2/index.test.ts
+++ src/11/2/index.test.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import testing from ".";
 
 describe("11/2", () => {
-  test("should pass with example input", () => {
+  test.skip("should pass with example input", () => {
     const input = fs.readFileSync(`${__dirname}/../example.txt`, { encoding: "utf-8" });
     const output = 1030;
 
@@ -12,16 +12,16 @@ describe("11/2", () => {
     expect(result).toStrictEqual(output);
   });
 
-  test("should log the result", () => {
+  test.skip("should log the result", () => {
     const input = fs.readFileSync(`${__dirname}/../input.txt`, { encoding: "utf-8" });
 
     const result = testing(input);
     console.log(result);
   });
 
-  test.skip("should pass after refactor", () => {
+  test("should pass after refactor", () => {
     const input = fs.readFileSync(`${__dirname}/../input.txt`, { encoding: "utf-8" });
-    const output = 0;
+    const output = 726820169514;
 
     const result = testing(input);
 
